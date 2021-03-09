cdfc66378ddf76b85b18f9323bc8910ca496bd7e
leejy001 <greatness007@naver.com>
2021-03-09 21:35:39 +0900

    상품 상세 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 import MainpageComponent from "./main";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
+
 function App() {
-  return <MainpageComponent></MainpageComponent>;
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="/images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainpageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
 }
 
 export default App;
--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Mainpage() {
   const [products, setProducts] = React.useState([]);
@@ -20,20 +21,15 @@ function Mainpage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
                 <div>
                   <img className="product-img" src={product.imageUrl} />
                 </div>
@@ -48,12 +44,11 @@ function Mainpage() {
                     <span>{product.seller}</span>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
